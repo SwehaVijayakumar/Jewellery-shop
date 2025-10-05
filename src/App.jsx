d130687cca5cf7c0b3ff5287552c22a0633e8b72
SwehaVijayakumar <sweha2028@gmail.com>
2025-10-05 19:52:03 +0530

    Updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -8,18 +8,25 @@ import Categories from "./pages/Categories";
 import Login from "./pages/Login";
 import Wishlist from "./pages/Wishlist";
 import Cart from "./pages/Cart";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        {/* Default Home Route */}
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
+
+        {/* Other Pages */}
         <Route path="/shop" element={<Shop />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/login" element={<Login />} />
         <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+
+        {/* Redirect unknown URLs to Home */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </Router>
