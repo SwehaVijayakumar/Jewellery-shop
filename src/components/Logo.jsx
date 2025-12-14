d766770f1098f3d7299373f959e4b350937c4808
SwehaVijayakumar <sweha2028@gmail.com>
2025-12-14 22:37:13 +0530

    Updated project

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import "./Logo.css";
 
+const base = import.meta.env.BASE_URL;
+
 function Logo() {
   return (
     <Link to="/" className="logo-link">
       <img
-        src="images/logo.png"
-        alt="Vinuriya Jewellery"
+        src={`${base}images/logo.png`}
+        alt="Vinuriya Jewellery Logo"
         className="site-logo"
       />
     </Link>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Jewellery-shop/",
   plugins: [react()],
-})
+  base: "/Jewellery-shop/", // 👈 EXACT GitHub repo name
+});
